b459109d8394535fa7a2f536eb0504beeed1079f
punithsabitha245 <punith.sabitha@gmail.com>
2025-10-07 21:00:11 +0530

    Add files via upload

---
+++ Robot Introduction.py
@@ -0,0 +1,20 @@
+class robot:
+    name = "Tom"
+    
+    def introduction(self):
+        print("Hi, I am a robot and my name is", self.name)
+
+
+ob = robot()
+ob.introduction()
+
+
+class student:
+    name = "Jerry"
+
+    def introduction(self):
+        print("Hi, I am a robot and my name is", self.name)
+
+
+ob = student()
+ob.introduction()
